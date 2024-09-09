23fa45d340848d0d9f96c291aadb1b3567b8542e
nototria <humphrey0709@gmail.com>
2024-09-10 00:44:14 +0800

    auto install font

--- install.sh
+++ install.sh
@@ -127,30 +127,26 @@ stow .
 
 # install Nerd font
 echo "installing Nerd fonts."
-#!/bin/bash
-
 declare -a fonts=(
-    JetBrainsMono
-    Meslo
+FiraCode
+FiraMono
+JetBrainsMono
+Meslo
 )
-
-version='2.1.0'
+version=$(curl -s 'https://api.github.com/repos/ryanoasis/nerd-fonts/releases/latest' | jq -r '.name')
 fonts_dir="${HOME}/.local/share/fonts"
-
 if [[ ! -d "$fonts_dir" ]]; then
-    mkdir -p "$fonts_dir"
+mkdir -p "$fonts_dir"
 fi
-
 for font in "${fonts[@]}"; do
-    zip_file="${font}.zip"
-    download_url="https://github.com/ryanoasis/nerd-fonts/releases/download/v${version}/${zip_file}"
-    echo "Downloading $download_url"
-    wget "$download_url"
-    unzip "$zip_file" -d "$fonts_dir"
-    rm "$zip_file"
+zip_file="${font}.zip"
+download_url="https://github.com/ryanoasis/nerd-fonts/releases/download/${version}/${zip_file}"
+echo "Downloading $download_url"
+wget "$download_url"
+unzip "$zip_file" -d "$fonts_dir"
+rm "$zip_file"
 done
-
-find "$fonts_dir" -name '*Windows Compatible*' -delete
+find "$fonts_dir" -name 'Windows Compatible' -delete
 fc-cache -fv
 
 echo "Installation complete."
