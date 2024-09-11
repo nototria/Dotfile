dddce4773f3fe600c3158eb8c6c989978b402e92
nototria <humphrey0709@gmail.com>
2024-09-12 00:39:46 +0800

    add installation of fzf

--- install.sh
+++ install.sh
@@ -84,6 +84,15 @@ else
     echo "zplugin_requirement.txt not found, skipping Zsh plugin installation."
 fi
 
+# Install fzf after all Zsh plugins are installed
+if [ ! -d "$HOME/.fzf" ]; then
+    echo "Installing fzf..."
+    git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+    ~/.fzf/install
+else
+    echo "fzf is already installed."
+fi
+
 # Check if requirements.txt exists
 if [ -f "requirements.txt" ]; then
     echo "Installing packages from requirements.txt..."
