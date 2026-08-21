c00d7801b5f7d4ef9b1e20a5a0dd17766db9c5d5
nototria <131363637+nototria@noreply.github.com>
2026-08-21 15:43:14 +0800

    chore: update treesitter syntax for higher nvim version

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,15 +1,22 @@
+local parsers = { "c", "cpp", "lua", "markdown", "markdown_inline", "python", "vim", "vimdoc" }
+local filetypes = { "c", "cpp", "lua", "markdown", "python", "vim", "vimdoc" }
+
 return {
   {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    event = { "BufReadPost", "BufNewFile" },
+    "neovim-treesitter/nvim-treesitter",
+    dependencies = { "neovim-treesitter/treesitter-parser-registry" },
+    lazy = false,
+    build = function()
+      require("nvim-treesitter").install(parsers):wait(300000)
+    end,
     config = function()
-      require("nvim-treesitter").setup({
-        highlight = { enable = true },
-        indent = { enable = true },
-        ensure_installed = { "c", "cpp", "lua", "python", "vim", "vimdoc" },
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = filetypes,
+        callback = function()
+          vim.treesitter.start()
+          vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+        end,
       })
     end,
   },
 }
-
--- .zshrc
+++ .zshrc
@@ -30,7 +30,7 @@ alias r="sh ./run.sh"
 # init
 source $ZSH/oh-my-zsh.sh
 
-eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+# eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 
 [ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
 
